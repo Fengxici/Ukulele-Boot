54732a82f0e3c3e1fba0e2d4378007736b7a134d
910980941@qq.com <910980941@qq.com>
2019-10-15 22:50:21 +0800

    依赖调整

--- trace/src/main/java/timing/ukulele/boot/trace/TraceApplication.java
+++ trace/src/main/java/timing/ukulele/boot/trace/TraceApplication.java
@@ -3,14 +3,12 @@ package timing.ukulele.boot.trace;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import zipkin2.server.internal.EnableZipkinServer;
-import zipkin2.server.internal.RegisterZipkinHealthIndicators;
 
 @SpringBootApplication
 @EnableZipkinServer
 public class TraceApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(TraceApplication.class)
-                .listeners(new RegisterZipkinHealthIndicators())
                 .properties("spring.config.name=zipkin-server")
                 .run(args);
     }
