50223443223e85cfce23c418382d37bceaac7463
吕自聪 <lvzc@flline88.com>
2021-07-26 11:25:23 +0800

    版本升级及优化

--- trace/src/main/java/timing/ukulele/boot/trace/TraceApplication.java
+++ trace/src/main/java/timing/ukulele/boot/trace/TraceApplication.java
@@ -1,16 +1,18 @@
 package timing.ukulele.boot.trace;
 
+import com.linecorp.armeria.spring.ArmeriaAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import zipkin2.server.internal.EnableZipkinServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArmeriaAutoConfiguration.class)
 @EnableZipkinServer
 public class TraceApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(TraceApplication.class)
-                .properties("spring.config.name=zipkin-server")
-                .run(args);
+        SpringApplication.run(TraceApplication.class,args);
+//        new SpringApplicationBuilder(TraceApplication.class)
+//                .properties("spring.config.name=zipkin-server")
+//                .run(args);
     }
 }
 
